be7693d0b539f60012ee6d7edcf03a93e3ffa651
mdmorris <megmorris121@gmail.com>
2022-04-04 15:52:45 -0500

    adding processor and csv files

---
+++ CoffeaJERCProcessor.py
@@ -0,0 +1,210 @@
+#CoffeaJERCProcessor.py
+
+import copy
+import scipy.stats as ss
+from coffea import hist, processor, nanoevents
+from coffea import util
+import numpy as np
+import itertools
+import pandas as pd
+from numpy.random import RandomState
+from coffea.jetmet_tools import FactorizedJetCorrector, JetCorrectionUncertainty
+from coffea.jetmet_tools import JECStack, CorrectedJetsFactory
+from coffea.lookup_tools import extractor
+
+
+
+import awkward as ak
+#from coffea.nanoevents.methods import nanoaod
+from coffea.nanoevents.methods import candidate
+from coffea.nanoevents.methods import vector
+
+
+
+manual_bins = [400, 500, 600, 800, 1000, 1500, 2000, 3000, 7000, 10000]
+
+ptbins = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 20, 23, 27, 30, 35, 40, 45, 57, 72, 90, 120, 
+        150, 200, 300, 400, 550, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 10000 ])
+
+
+etabins =   np.array([-4.889,  -4.716,  -4.538,  -4.363,  -4.191,  -4.013,  -3.839,  -3.664,  -3.489,
+           -3.314,  -3.139,  -2.964,  -2.853,  -2.65,  -2.5,  -2.322,  -2.172,  -2.043,  -1.93,  -1.83,
+           -1.74,  -1.653,  -1.566,  -1.479,  -1.392,  -1.305,  -1.218,  -1.131,  -1.044,  -0.957,  -0.879,
+           -0.783,  -0.696,  -0.609,  -0.522,  -0.435,  -0.348,  -0.261,  -0.174,  -0.087,  0,  0.087,  0.174,
+           0.261,  0.348,  0.435,  0.522,  0.609,  0.696,  0.783,  0.879,  0.957,  1.044,  1.131,  1.218,
+           1.305,  1.392,  1.479,  1.566,  1.653,  1.74,  1.83,  1.93,  2.043,  2.172,  2.322,  2.5,  2.65,
+           2.853,  2.964,  3.139,  3.314,  3.489,  3.664,  3.839,  4.013,  4.191,  4.363,  4.538,  4.716,
+           4.889, 5.191 ])
+
+
+
+
+"""@TTbarResAnaHadronic Package to perform the data-driven mistag-rate-based ttbar hadronic analysis. 
+"""
+class Processor(processor.ProcessorABC):
+    def __init__(self):
+        
+        
+        dataset_axis = hist.Cat("dataset", "Primary dataset")
+        cats_axis = hist.Cat("anacat", "Analysis Category")
+       
+        jetpt_axis = hist.Bin("pt", r"$p_T$", ptbins)
+        ptresponse_axis = hist.Bin("ptresponse", "RECO / GEN response", 100, 0, 5)
+        jetmass_axis = hist.Bin("jetmass", r"Jet $m$ [GeV]", 50, 0, 500)
+        #jetpt_axis = hist.Bin("jetpt", r"Jet $p_{T}$ [GeV]", 50, 0, 5000)
+        ttbarmass_axis = hist.Bin("ttbarmass", r"$m_{t\bar{t}}$ [GeV]", 50, 0, 5000)
+        jeteta_axis = hist.Bin("jeteta", r"Jet $\eta$", etabins)
+        jetphi_axis = hist.Bin("jetphi", r"Jet $\phi$", 50, -np.pi, np.pi)
+        jety_axis = hist.Bin("jety", r"Jet $y$", 50, -3, 3)
+        jetdy_axis = hist.Bin("jetdy", r"Jet $\Delta y$", 50, 0, 5)
+        manual_axis = hist.Bin("jetp", r"Jet Momentum [GeV]", manual_bins)
+        tagger_axis = hist.Bin("tagger", r"deepTag", 50, 0, 1)
+        tau32_axis = hist.Bin("tau32", r"$\tau_3/\tau_2$", 50, 0, 2)
+        
+        subjetmass_axis = hist.Bin("subjetmass", r"SubJet $m$ [GeV]", 50, 0, 500)
+        subjetpt_axis = hist.Bin("subjetpt", r"SubJet $p_{T}$ [GeV]", 50, 0, 2000)
+        subjeteta_axis = hist.Bin("subjeteta", r"SubJet $\eta$", 50, -4, 4)
+        subjetphi_axis = hist.Bin("subjetphi", r"SubJet $\phi$", 50, -np.pi, np.pi)
+
+        self._accumulator = processor.dict_accumulator({
+            'ptresponse': hist.Hist("Counts", dataset_axis, jetpt_axis, jeteta_axis, ptresponse_axis),
+#             'corrected_ptresponse': hist.Hist("Counts", dataset_axis, jetpt_axis, jeteta_axis, ptresponse_axis),
+
+            'jetmass': hist.Hist("Counts", dataset_axis, cats_axis, jetmass_axis),
+            
+            'jetpt':     hist.Hist("Counts", dataset_axis, jetpt_axis),
+            'jeteta':    hist.Hist("Counts", dataset_axis, jeteta_axis),
+            'jetphi':    hist.Hist("Counts", dataset_axis, jetphi_axis),
+            
+            
+            'cutflow': processor.defaultdict_accumulator(int),
+            
+        })
+        
+        
+        ext = extractor()
+        ext.add_weight_sets([
+            "* * Summer20_UL18_MC_L1FastJet_AK4PFchs.jec.txt",
+        ])
+        ext.finalize()
+
+        jec_stack_names = ["Summer20_UL18_MC_L1FastJet_AK4PFchs"]
+
+        evaluator = ext.make_evaluator()
+        
+        print(evaluator)
+        print(evaluator.keys())
+        
+        jec_inputs = {name: evaluator[name] for name in jec_stack_names}
+        jec_stack = JECStack(jec_inputs)
+        ### more possibilities are available if you send in more pieces of the JEC stack
+        # mc2016_ak8_jxform = JECStack(["more", "names", "of", "JEC parts"])
+        
+        self.corrector = FactorizedJetCorrector(
+            Summer20_UL18_MC_L1FastJet_AK4PFchs=evaluator['Summer20_UL18_MC_L1FastJet_AK4PFchs'],
+        )
+#         uncertainties = JetCorrectionUncertainty(
+#             Summer20_UL18_MC_L1FastJet_AK4PFchs=evaluator['Summer20_UL18_MC_L1FastJet_AK4PFchs']
+#         )
+
+
+        self.name_map = jec_stack.blank_name_map
+        self.name_map['JetPt'] = 'pt'
+        self.name_map['JetMass'] = 'mass'
+        self.name_map['JetEta'] = 'eta'
+        self.name_map['JetA'] = 'area'
+        self.name_map['ptGenJet'] = 'pt_gen'
+        self.name_map['ptRaw'] = 'pt_raw'
+        self.name_map['massRaw'] = 'mass_raw'
+        self.name_map['Rho'] = 'rho'
+        
+        
+        self.jet_factory = CorrectedJetsFactory(self.name_map, jec_stack)
+
+        print(dir(evaluator))
+        print()
+
+            
+    @property
+    def accumulator(self):
+        return self._accumulator
+
+    def process(self, events):
+        
+        output = self.accumulator.identity()
+        
+        dataset = events.metadata['dataset']
+     
+ 
+        selectedEvents = events[
+            (ak.num(events.Jet) > 2)
+        ]
+    
+
+        # change to selectedEvents
+        GenJets = events.GenJet[:,0:2]
+        jets = events.Jet 
+        
+        
+        jets['pt_raw'] = (1 - jets['rawFactor']) * jets['pt']
+        jets['mass_raw'] = (1 - jets['rawFactor']) * jets['mass']
+        jets['pt_gen'] = ak.values_astype(ak.fill_none(jets.matched_gen.pt, 0), np.float32)
+        jets['rho'] = ak.broadcast_arrays(events.fixedGridRhoFastjetAll, jets.pt)[0]
+
+
+        events_cache = events.caches[0]
+
+        corrected_jets = self.jet_factory.build(jets, lazy_cache=events_cache)
+
+
+#         print('JES UP pt ratio',corrected_jets.JES_jes.up.pt/corrected_jets.pt_raw)
+#         print('JES DOWN pt ratio',corrected_jets.JES_jes.down.pt/corrected_jets.pt_raw)
+       
+        jetpt = jets.pt
+        corrected_jetpt = corrected_jets.pt
+   
+
+        #jets = jets[~ak.is_none(jets.matched_gen)]
+        #jets = jets[~ak.is_none(jets.pt)]
+
+
+        matchedJets = ak.cartesian([GenJets, corrected_jets])
+        
+        deltaR = matchedJets.slot0.delta_r(matchedJets.slot1)
+        
+        matchedJets = matchedJets[deltaR < 0.2]
+#         corrected_jets = corrected_jets[deltaR < 0.2]
+        
+        matchedJets = matchedJets[ak.num(matchedJets) > 0]
+
+        jetpt = matchedJets.slot1.pt
+        jeteta = matchedJets.slot1.eta
+
+
+        
+        matched_genjetpt = matchedJets.slot0.pt
+        matched_genjeteta = matchedJets.slot0.eta
+        
+        
+        ptresponse = jetpt / matched_genjetpt
+#         corrected_ptresponse = corrected_jetpt / matched_genjetpt
+        
+        output['jetpt'].fill(dataset = dataset, 
+                        pt = ak.flatten(matched_genjetpt))
+
+        output['jeteta'].fill(dataset = dataset, 
+                        jeteta = ak.to_numpy(ak.flatten(jeteta), allow_missing=True))
+       
+
+        output['ptresponse'].fill(dataset=dataset, pt=ak.to_numpy(ak.flatten(matched_genjetpt), allow_missing=True),
+                        jeteta=ak.to_numpy(ak.flatten(matched_genjeteta), allow_missing=True),
+                        ptresponse=ak.to_numpy(ak.flatten(ptresponse), allow_missing=True))
+        
+        
+        return output
+
+    def postprocess(self, accumulator):
+        return accumulator
+
+
+
